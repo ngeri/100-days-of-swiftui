341cfed2fac8cbe3c2634f313e6ab79e570bf130
Gergely Nemeth <ngergo100@gmail.com>
2019-10-26 19:29:02 +0200

    Day 18

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -26,10 +26,10 @@ struct ContentView1: View {
                 .animation(
                     Animation.easeInOut(duration: 1)
                         .repeatForever(autoreverses: false)
-                )
+            )
         )
-        .onAppear {
-            self.animationAmount = 2
+            .onAppear {
+                self.animationAmount = 2
         }
         
     }
@@ -40,7 +40,7 @@ struct ContentView2: View {
     
     var body: some View {
         print(animationAmount)
-
+        
         return VStack {
             Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
             Spacer()
@@ -73,12 +73,141 @@ struct ContentView3: View {
     }
 }
 
+struct ContentView4: View {
+    @State private var enabled = false
+    
+    var body: some View {
+        Button("Tap Me") {
+            self.enabled.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? Color.red : Color.blue)
+        .animation(nil)
+        .foregroundColor(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+        
+    }
+}
+
+struct ContentView5: View {
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged {
+                        self.dragAmount = $0.translation
+                }
+                .onEnded { _ in
+                    withAnimation(.spring()) {
+                        self.dragAmount = .zero
+                    }
+                }
+        )
+    }
+}
+
+struct ContentView6: View {
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(self.letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(self.enabled ? Color.blue : Color.red)
+                    .offset(self.dragAmount)
+                    .animation(Animation.default.delay(Double(num)/20))
+            }
+        }
+    .gesture(
+        DragGesture()
+            .onChanged { self.dragAmount = $0.translation }
+            .onEnded { _ in
+                self.dragAmount = .zero
+                self.enabled.toggle()
+            }
+        )
+    }
+}
+
+struct ContentView7: View {
+    @State private var isShowing = false
+    
+    var body: some View {
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    self.isShowing.toggle()
+                }
+            }
+            
+            if isShowing {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+        }
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor)
+        .clipped()
+    }
+}
+
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
+struct ContentView8: View {
+    @State private var isShowing = false
+    
+    var body: some View {
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    self.isShowing.toggle()
+                }
+            }
+            
+            if isShowing {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView1()
             ContentView2()
             ContentView3()
+            ContentView4()
+            ContentView5()
+            ContentView6()
+            ContentView7()
         }
     }
 }
