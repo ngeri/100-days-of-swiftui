a53c5c71d3e5f5d48277610da11c8bc18c16beba
Gergely Nemeth <ngergo100@gmail.com>
2019-11-03 17:07:00 +0100

    Day 26

---
+++ Moonshot/Moonshot/MissionView.swift
@@ -0,0 +1,98 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Németh Gergely on 2019. 11. 03..
+//  Copyright © 2019. Németh Gergely. All rights reserved.
+//
+
+import SwiftUI
+
+struct PixelSize {
+    var sizeInPixel: CGFloat
+    var sizeInPoints: CGFloat {
+        sizeInPixel / UIScreen.main.scale
+    }
+}
+
+struct MissionView: View {
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
+    let mission: Mission
+    let astronauts: [CrewMember]
+    
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geo.size.width * 0.7)
+                    
+                    Text(self.mission.description)
+                        .padding()
+                    
+                    ForEach(self.astronauts, id: \.role) { crewMember in
+                        NavigationLink(destination: AstronautView(astronaut: crewMember.astronaut)) {
+                            HStack {
+                                Image(crewMember.astronaut.id)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 60, height: 60)
+                                    .clipShape(Circle())
+                                    .overlay(Circle()
+                                        .stroke(Color.primary, lineWidth: PixelSize(sizeInPixel: 1).sizeInPoints)
+                                    )
+                                
+                                VStack(alignment: .leading) {
+                                    Text(crewMember.astronaut.name)
+                                        .font(.subheadline)
+                                    
+                                    Text(crewMember.role)
+                                        .font(.footnote)
+                                        .foregroundColor(Color.secondary)
+                                }
+                                
+                                Spacer()
+                            }
+                            .padding(.horizontal)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                    
+                    Spacer(minLength: 25)
+                }
+            }
+        }
+        .navigationBarTitle(Text(mission.displayName), displayMode: .inline)
+    }
+    
+    init(mission: Mission, astronauts: [Astronaut]) {
+        self.mission = mission
+        
+        var matches = [CrewMember]()
+        
+        for member in mission.crew {
+            if let match = astronauts.first(where: { $0.id == member.name }) {
+                matches.append(CrewMember(role: member.role, astronaut: match))
+            } else {
+                fatalError("Missing member")
+            }
+        }
+        
+        self.astronauts = matches
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    static var previews: some View {
+        MissionView(mission: missions[0], astronauts: astronauts)
+    }
+}
