818a47a0318f77c20254b28b7649edb0620e0081
Gergely Nemeth <ngergo100@gmail.com>
2019-11-10 21:09:14 +0100

    Day 33

--- Habits/Habits/ActivitiesView.swift
+++ Habits/Habits/ActivitiesView.swift
@@ -24,6 +24,7 @@ struct ActivitiesView: View {
                                 Text(activity.description)
                                     .font(.caption)
                             }
+                            Spacer()
                             Text("\(activity.count)")
                         }
                         
