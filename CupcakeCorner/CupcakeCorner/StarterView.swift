61e8c9453513be6d21bd5de5e9d4bd48a4df3c7f
Gergely Nemeth <ngergo100@gmail.com>
2019-11-11 15:17:26 +0100

    Day 34

---
+++ CupcakeCorner/CupcakeCorner/StarterView.swift
@@ -0,0 +1,115 @@
+//
+//  StarterView.swift
+//  CupcakeCorner
+//
+//  Created by Németh Gergely on 2019. 11. 11..
+//  Copyright © 2019. Németh Gergely. All rights reserved.
+//
+
+import SwiftUI
+
+// MARK: - 1st
+final class User: ObservableObject, Codable {
+    @Published var name = "Paul Hudson"
+
+    enum CodingKeys: CodingKey {
+        case name
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+}
+
+// MARK: - 2nd
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct StarterView: View {
+    @State var results = [Result]()
+    
+    var body: some View {
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .onAppear(perform: loadData)
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    // we have good data – go back to the main thread
+                    DispatchQueue.main.async {
+                        // update our UI
+                        self.results = decodedResponse.results
+                    }
+
+                    // everything is good, so we can exit.
+                    return
+                }
+            }
+
+            // if we're still here it means there was a problem
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
+    }
+}
+
+// MARK: - 3rd
+struct SecondView: View {
+    @State var username = ""
+    @State var email = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
+                }
+                .disabled(disableForm)
+            }
+            
+        }
+    }
+}
+
+
+struct StarterView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
