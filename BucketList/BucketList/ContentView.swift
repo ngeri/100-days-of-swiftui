ecd007ae78b1e413d27a9530be5f853b07c681bf
Gergely Nemeth <ngergo100@gmail.com>
2019-12-05 19:34:11 +0100

    Day 58

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -10,43 +10,84 @@ import LocalAuthentication
 import MapKit
 import SwiftUI
 
+struct TargetView: View {
+    var body: some View {
+        Circle()
+            .fill(Color.blue)
+            .opacity(0.3)
+            .frame(width: 32, height: 32)
+    }
+}
+
+struct AddNewLocationButton: View {
+    let action: () -> Void
+    var body: some View {
+        Button(action: {
+            self.action()
+        }) {
+            Image(systemName: "plus")
+                .padding()
+                .background(Color.black.opacity(0.75))
+                .foregroundColor(.white)
+                .font(.title)
+                .clipShape(Circle())
+        }
+        .padding(.trailing)
+    }
+}
+
 struct ContentView: View {
     @State private var centerCoordinate = CLLocationCoordinate2D()
     @State private var locations = [CodableMKPointAnnotation]()
     @State private var selectedPlace: MKPointAnnotation?
     @State private var showingPlaceDetails = false
+    @State private var showingBiometricAuthenticationError = false
     @State private var showingEditScreen = false
     @State private var isUnlocked = false
 
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+
+    func alert(for selectedPlace: MKPointAnnotation?) -> Alert {
+        Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information."), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
+            self.showingEditScreen = true
+        })
+    }
+
+    func alert() -> Alert {
+        Alert(title: Text("Fucnk  you"), message: Text( "Missing place information."), dismissButton: .default(Text("OK")))
+    }
+
     var body: some View {
-        ZStack {
+        let customBinding = Binding(
+            get: {
+                return self.showingPlaceDetails || self.showingBiometricAuthenticationError
+            },
+            set: {
+                if !$0 {
+                    self.showingPlaceDetails = $0
+                    self.showingBiometricAuthenticationError = $0
+                }
+            }
+        )
+
+        return ZStack {
             if isUnlocked {
                 MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
                     .edgesIgnoringSafeArea(.all)
-                Circle()
-                    .fill(Color.blue)
-                    .opacity(0.3)
-                    .frame(width: 32, height: 32)
+                TargetView()
                 VStack {
                     Spacer()
                     HStack {
                         Spacer()
-                        Button(action: {
+                        AddNewLocationButton {
                             let newLocation = CodableMKPointAnnotation()
                             newLocation.title = "Example Location"
                             newLocation.coordinate = self.centerCoordinate
                             self.locations.append(newLocation)
                             self.selectedPlace = newLocation
                             self.showingEditScreen = true
-                        }) {
-                            Image(systemName: "plus")
                         }
-                        .padding()
-                        .background(Color.black.opacity(0.75))
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .clipShape(Circle())
-                        .padding(.trailing)
                     }
                 }
             } else {
@@ -59,10 +100,12 @@ struct ContentView: View {
                 .clipShape(Capsule())
             }
         }
-        .alert(isPresented: $showingPlaceDetails) {
-            Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information."), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
-                self.showingEditScreen = true
-            })
+        .alert(isPresented: customBinding) {
+            if self.showingPlaceDetails {
+                return self.alert(for: self.selectedPlace)
+            } else {
+                return self.alert()
+            }
         }
         .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
             if self.selectedPlace != nil {
@@ -111,7 +154,7 @@ struct ContentView: View {
                     if success {
                         self.isUnlocked = true
                     } else {
-                        // error
+                        self.showingBiometricAuthenticationError = true
                     }
                 }
             }
