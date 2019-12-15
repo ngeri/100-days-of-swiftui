28c4cca8bb740b9b2a8416bd5ca7216033129680
Gergely Nemeth <ngergo100@gmail.com>
2019-12-15 20:45:54 +0100

    Day 68

--- HotProspects/HotProspects/ProspectsView.swift
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -6,10 +6,12 @@
 //  Copyright © 2019. Németh Gergely. All rights reserved.
 //
 
+import CodeScanner
 import SwiftUI
 
 struct ProspectsView: View {
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
 
     enum FilterType {
         case none, contacted, uncontacted
@@ -27,18 +29,23 @@ struct ProspectsView: View {
                         Text(prospect.emailAddress)
                             .foregroundColor(.secondary)
                     }
+                    .contextMenu {
+                        Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted" ) {
+                            self.prospects.toggle(prospect)
+                        }
+                    }
                 }
             }
                 .navigationBarTitle(title)
                 .navigationBarItems(trailing: Button(action: {
-                    let prospect = Prospect()
-                    prospect.name = "Paul Hudson"
-                    prospect.emailAddress = "paul@hackingwithswift.com"
-                    self.prospects.people.append(prospect)
+                    self.isShowingScanner = true
                 }) {
                     Image(systemName: "qrcode.viewfinder")
                     Text("Scan")
                 })
+                .sheet(isPresented: $isShowingScanner) {
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: self.handleScan)
+                }
         }
     }
 
@@ -62,6 +69,23 @@ struct ProspectsView: View {
         case .uncontacted:
             return prospects.people.filter { !$0.isContacted }
         }
+    }
+
+    func handleScan(result: Result<String, CodeScannerView.ScanError>) {
+       self.isShowingScanner = false
+       switch result {
+       case .success(let code):
+           let details = code.components(separatedBy: "\n")
+           guard details.count == 2 else { return }
+
+           let person = Prospect()
+           person.name = details[0]
+           person.emailAddress = details[1]
+
+           self.prospects.people.append(person)
+       case .failure(let error):
+        print("Scanning failed with \(error.localizedDescription)")
+       }
     }
 }
 
