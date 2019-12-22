972cce8b244decbbe04c6eff402449a9e1a74041
Gergely Nemeth <ngergo100@gmail.com>
2019-12-22 17:46:49 +0100

    Day 90

--- Flashzilla/Flashzilla/Card.swift
+++ Flashzilla/Flashzilla/Card.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019. Németh Gergely. All rights reserved.
 //
 
-struct Card {
+struct Card: Codable {
     let prompt: String
     let answer: String
 
--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 
 struct CardView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityEnabled) var accessibilityEnabled
     @State var isShowingAnswer = false
     @State var offset = CGSize.zero
+    let feedback = UINotificationFeedbackGenerator()
     let card: Card
     var removal: (() -> Void)? = nil
 
@@ -34,13 +36,18 @@ struct CardView: View {
                 .shadow(radius: 10)
 
             VStack {
-                Text(card.prompt)
-                    .font(.largeTitle)
+                if accessibilityEnabled {
+                    Text(isShowingAnswer ? card.answer : card.prompt)
+                        .font(.largeTitle)
+                } else {
+                    Text(card.prompt)
+                        .font(.largeTitle)
 
-                if isShowingAnswer {
-                    Text(card.answer)
-                        .font(.title)
-                        .foregroundColor(.secondary)
+                    if isShowingAnswer {
+                        Text(card.answer)
+                            .font(.title)
+                            .foregroundColor(.secondary)
+                    }
                 }
             }
             .padding(20)
@@ -50,14 +57,19 @@ struct CardView: View {
         .rotationEffect(.degrees(Double(offset.width / 5)))
         .offset(x: offset.width * 5, y: 0)
         .opacity(2 - Double(abs(offset.width / 50)))
+        .accessibility(addTraits: .isButton)
         .gesture(
             DragGesture()
                 .onChanged { gesture in
                     self.offset = gesture.translation
+                    self.feedback.prepare()
                 }
 
                 .onEnded { _ in
                     if abs(self.offset.width) > 100 {
+                        if self.offset.width < 0 {
+                            self.feedback.notificationOccurred(.error)
+                        }
                         self.removal?()
                     } else {
                         self.offset = .zero
@@ -67,6 +79,7 @@ struct CardView: View {
         .onTapGesture {
             self.isShowingAnswer.toggle()
         }
+        .animation(.spring())
     }
 }
 
